e3cdee3a2914b5280c435f09b28ac3134da29909
DanielGuevara2008 <daniel.guevara-r@uniminuto.edu.co>
2026-03-06 20:57:06 -0500

    Primer programa

---
+++ semana3/ejer1.py
@@ -0,0 +1,19 @@
+a=20
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+#esta de abajo es el residuo de la división
+print(a%b)
+print("el resultado de la suma entre ",a, "y", b ,"es =", a+b )
+print("el resultado de la resta entre ",a, "y", b ,"es =", a-b )
+print("el resultado de la multiplicación entre ",a, "y", b ,"es =", a*b )
+print("el resultado de la división entre ",a, "y", b ,"es =", a/b )
+print("la diferencia entre ",a, "y", b ,"es =", a//b )
+print("el residuo de la división entre ",a, "y", b ,"es =", a%b )
+#comparación
+print(a<b)
+print(a>b)
+print(a==b)
